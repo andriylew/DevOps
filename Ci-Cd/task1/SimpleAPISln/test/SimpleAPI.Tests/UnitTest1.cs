59934caa27e4b9b78fb7fcbe3c75c13dc34e9135
Andrii Levytskyi <andriylew29@gmail.com>
2022-10-19 19:37:04 +0300

    edit test

--- Ci-Cd/task1/SimpleAPISln/test/SimpleAPI.Tests/UnitTest1.cs
+++ Ci-Cd/task1/SimpleAPISln/test/SimpleAPI.Tests/UnitTest1.cs
@@ -2,19 +2,21 @@ using Sysytem;
 using Xunit;
 using SimpleAPI.Controllers;
 
-public class UnitTest1
+
+namespace SimpleAPI.Test
 {
-
-	ValuesController controller = new ValuesController();
-	[Fact]
-	public void GetReturnsCorrectNumber()
+	public class UnitTest1
 	{
-	  var returnValue = controller.Get(1);
-	  Assert.Equal("Les Jackson", returnValue.Value);
+		ValuesController controller = new ValuesController();
+		[Fact]
+		public void GetReturnsCorrectNumber()
+		{
+		  var returnValue = controller.Get(1);
+		  Assert.Equal("Les Jackson", returnValue.Value);
+		}
+    	[Fact]
+    	public void Test1()
+    	{
+    	}
 	}
-    [Fact]
-    public void Test1()
-    {
-
-    }
 }
